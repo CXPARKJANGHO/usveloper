d251a4a2900522bd465941e1406cda6a07378dae
LeeSangJin <sangjin2041@gmail.com>
2020-08-25 20:13:52 +0900

    react-router-dom

--- reactEX/src/App.tsx
+++ reactEX/src/App.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import "./App.css";
-import App1 from "./App1";
+import React from 'react';
+import './App.css';
+import App1 from './App1';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import EventApp from './eventEX/EventApp';
+import EventApp2 from './eventEX2/EventApp';
 
 // 기본
 //* 정보
@@ -14,9 +17,34 @@ import App1 from "./App1";
  */
 function App() {
     return (
-        <div className='App'>
-            <App1 />
-        </div>
+        <Router>
+            <div className='App'>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/eventApp1'>eventApp1</Link>
+                        </li>
+                        <li>
+                            <Link to='/eventApp2'>eventApp2</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path='/eventApp1'>
+                        <EventApp />
+                    </Route>
+                    <Route path='/eventApp2'>
+                        <EventApp2 />
+                    </Route>
+                    <Route path='/'>
+                        <App1 />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 export default App;
--- reactEX/src/index.tsx
+++ reactEX/src/index.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import EventApp from "./eventEX/EventApp";
-import EventApp2 from "./eventEX2/EventApp";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import EventApp from './eventEX/EventApp';
+import EventApp2 from './eventEX2/EventApp';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
-        APP
-        <App /> EventAPP
-        <EventApp /> EventAPP2
-        <EventApp2 />
+        <App />
     </React.StrictMode>,
-    document.getElementById("root")
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
