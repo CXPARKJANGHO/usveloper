355840abbd7d1c66f42ba953598cea6ee9a04995
CXPARKJANGHO <lebedeutsch@gmail.com>
2020-09-04 14:28:14 +0900

    ..

---
+++ reactEX/src/eventEX2/LoginTest.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useReducer } from "react";
+import BaseButton from "../base/base_button";
+import "./EventApp.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// 기본
+//* 정보
+//! 경고
+//? 질문
+//TODO TODO
+//
+/**
+ *
+ * @param a
+ */
+function LoginTest() {
+    return <div>LOGIN</div>;
+}
+export default LoginTest;
